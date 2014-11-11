c74829648ff2f9bb75f49186373b38da19790e06
Mathieu Van der Haegen <m@dcy.io>
2014-11-11 17:07:30 +0100

    correct deploy

--- deploy.sh
+++ deploy.sh
@@ -118,7 +118,7 @@ fi
 if [ -e "$DEPLOYMENT_SOURCE/bower.json" ]; then
   eval $NPM_CMD install bower
   exitWithMessageOnError "installing bower failed"
-  ./node_modules/.bin/bower install --allow-root
+  ./node_modules/.bin/bower install
   exitWithMessageOnError "bower failed"
 fi
 
@@ -126,7 +126,7 @@ fi
 if [ -e "$DEPLOYMENT_SOURCE/Gruntfile.js" ]; then
   eval $NPM_CMD install grunt-cli
   exitWithMessageOnError "installing grunt failed"
-  ./node_modules/.bin/grunt --no-color clean
+  ./node_modules/.bin/grunt --no-color clean common dist
   exitWithMessageOnError "grunt failed"
 fi
 
