2cabf9455c8ece20c10faf0b77017eb8b4def70c
Mathieu Van der Haegen <m@dcy.io>
2014-11-11 16:33:11 +0100

    bcrypt fix

--- routes.js
+++ routes.js
@@ -35,6 +35,7 @@ exports = module.exports = function(app, passport) {
   app.get('/contact/', require('./views/contact/index').init);
   app.post('/contact/', require('./views/contact/index').sendMessage);
 
+
   //sign up
   app.get('/signup/', require('./views/signup/index').init);
   app.post('/signup/', require('./views/signup/index').signup);
