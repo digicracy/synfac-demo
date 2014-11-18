a24a79547c359f2e2f740ad0465938be0146de6d
Mathieu Van der Haegen <m@dcy.io>
2014-11-18 12:28:30 +0100

    var

--- app.js
+++ app.js
@@ -24,7 +24,8 @@ app.config = config;
 app.server = http.createServer(app);
 
 //setup mongoose
-dbopts ={server:{socketOptions:{ keepAlive: 1 }},replset:{socketOptions:{ keepAlive: 1 }}};
+var dbopts = {server:{socketOptions:{keepAlive:1}},replset:{socketOptions:{keepAlive:1}}};
+
 app.db = mongoose.createConnection(config.mongodb.uri,dbopts);
 app.db.on('error', console.error.bind(console, 'mongoose connection error: '));
 app.db.once('open', function () {
